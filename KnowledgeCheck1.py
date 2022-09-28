789bffbef4d4056a7cb2513dfeb6407887268ca5
Adam Conklin <conklin19@gmail.com>
2022-09-27 20:42:56 -0400

    first two parts

--- KnowledgeCheck1.py
+++ KnowledgeCheck1.py
@@ -3,3 +3,14 @@
 ### Data Analytics 1 September-November 2022 ###
 
 ## Knowledge Check ###
+
+print("Hello World")
+
+BigTenTeams = ['Illinois', 'Indiana', 'Iowa', 'Maryland', 'Michigan', 'Michigan State',
+                'Minnesota', 'Nebraska', 'Northwestern', 'Ohio State', 'Penn State', 'Purdue', 
+                'Rutgers', 'Wisconsin']
+### Alphabetized List of Big Ten Schools ###
+
+print(BigTenTeams[11])
+
+#Print a team on the list. Selectee

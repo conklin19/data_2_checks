f79b348d3367b4ab65df73ee7bcf69f64c032afd
conklin19 <conklin19@gmail.com>
2022-11-06 04:57:20 -0500

    Imported CSV and Plotted

--- kc_2.py
+++ kc_2.py
@@ -3,4 +3,23 @@
 ### Data Analysis 1 ###
 ### September 2022 Cohort ###
 
-### Knowlege Check 2 ###
+### Knowlege Check 2 ##
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(r'C:\Users\conkl\OneDrive\Documents\PythonScripts\data_1_checks\assets\KC_2_data.csv')
+df.head()
+
+print(df)
+
+plt.plot(df.Game, df.Points_Scored)
+plt.show()
+
+
+
+
+
+
+

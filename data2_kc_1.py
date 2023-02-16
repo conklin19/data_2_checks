a2f535b65b6328c050737c9f89fd3365ef585b2b
conklin19 <conklin19@gmail.com>
2023-02-15 19:22:22 -0500

    connected to API

--- data2_kc_1.py
+++ data2_kc_1.py
@@ -2,3 +2,11 @@
 ## Code Louisville ##
 ## Data Analytics 2 ##
 ## Knowledge Check 1 ##
+
+import requests
+
+dataUSA = requests.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+print(dataUSA.json())
+
+
+

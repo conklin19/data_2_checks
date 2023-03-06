15a04a8e0a6d9d69ad3725a472d34741d8443392
conklin19 <conklin19@gmail.com>
2023-03-06 18:25:10 -0500

    imported API

--- data2_kc2.py
+++ data2_kc2.py
@@ -5,3 +5,16 @@
 
 ### Knowledge Check 2: Data Cleaning ###
 
+import requests
+import pandas as pd
+
+
+## Pull in United States Population Data from Knowledge Check 1 ###
+
+dataUSA = requests.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+
+### Convert the imported API json to a pandas dataframe ###
+
+df = pd.json_normalize(dataUSA.json()['data'])
+
+print(df)

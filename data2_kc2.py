f5eb06bc59e010d03175170f6824cc91106fa1d8
conklin19 <conklin19@gmail.com>
2023-03-06 18:59:50 -0500

    changed column names and finished

--- data2_kc2.py
+++ data2_kc2.py
@@ -17,7 +17,9 @@ dataUSA = requests.get('https://datausa.io/api/data?drilldowns=Nation&measures=P
 
 df = pd.json_normalize(dataUSA.json()['data'])
 
+print(df)
 
+### Make a Copy of the original dataframe ###
 clean_df = df
 
 ### First Clean: Delete redundant column 'ID Year' because we don't need the year twice ###
@@ -25,9 +27,18 @@ clean_df = clean_df.drop(['ID Year'], axis = 1)
 
 print(clean_df)
 
+### Second Clean: Replace erroneous values to a simple 1 ###
 clean_df['ID Nation'].replace('01000US', '1', inplace=True)
 
+###Third Clean: Put full name of the country in instead of just United States ###
 clean_df['Nation'].replace('United States', 'United States of America', inplace=True)
+
+### Fourth Clean: Change erroneous Slug Nation column to an abbreviation of the country name ###
+clean_df.rename(columns = {'Slug Nation':'Abbreviation'}, inplace=True)
+
+### Fifth Clean: change values in Abbrevation column to USA ###
+clean_df['Abbreviation'].replace('united-states','USA', inplace=True)
+
 
 print(clean_df)
 

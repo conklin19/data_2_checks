ea7580530bd32dde08d0c6e315058c339494961e
conklin19 <conklin19@gmail.com>
2023-03-24 12:51:15 -0400

    imported sql database

--- kenpomfinalfour.py
+++ kenpomfinalfour.py
@@ -7,7 +7,16 @@
 ## Advanced KenPom College Basketball Analytics ###
 
 import pandas as pd
+import sqlite3
 
 ## Requirement 1: Loading Data ###
 ## Set Up A Local Database and read data in with SQLite ##
 
+conn = sqlite3.connect("assets/original.db")
+c = conn.cursor()
+
+sql = pd.read_sql_query("SELECT * FROM info", conn)
+
+df = pd.DataFrame(sql, columns = ["Year", "Team", "KPrank", "KPvalue", "Conference", "Champion"])
+
+print(df)

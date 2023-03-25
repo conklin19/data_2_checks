8f1a297d19b1e86e64a56d1f0c00e872f605e006
conklin19 <conklin19@gmail.com>
2023-03-25 00:05:09 -0400

    read in SQL database 2

--- kenpomfinalfour.py
+++ kenpomfinalfour.py
@@ -15,8 +15,11 @@ import sqlite3
 conn = sqlite3.connect("assets/original.db")
 c = conn.cursor()
 
-sql = pd.read_sql_query("SELECT * FROM info", conn)
+sql = pd.read_sql_query("SELECT * FROM kenpom", conn)
 
 df = pd.DataFrame(sql, columns = ["Year", "Team", "KPrank", "KPvalue", "Conference", "Champion"])
 
-print(df)
+sql_2 = pd.read_sql_query("SELECT * FROM newtable", conn)
+
+df2 = pd.DataFrame(sql_2, columns = ["year", "team", "wins", "losses", "coach", "nickname"])
+print(df2)

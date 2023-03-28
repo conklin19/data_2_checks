8c6c17f010fef6abfee8ec30693090bc640a7a35
conklin19 <conklin19@gmail.com>
2023-03-28 18:17:27 -0400

    revising pandas merge

--- kenpomfinalfour.py
+++ kenpomfinalfour.py
@@ -23,22 +23,11 @@ df = pd.DataFrame(sql, columns = ["Year", "Team", "KPrank", "KPvalue", "Conferen
 sql_2 = pd.read_sql_query("SELECT * FROM newtable", conn)
 
 df2 = pd.DataFrame(sql_2, columns = ["Year", "Team", "Wins", "Losses", "Coach", "Nickname"])
-print(df2)
+
 
 ### Requirement 2: Clean your data and perform a pandas merge with two data sets ###
 ### then calculate some new values based on the new data set ###
-combined = sql.merge(sql_2, left_on= 'Year', right_on='Year')
-
-### Clean by deleting duplicate/redundant rows ###
-combined.drop(['id_x','id_y', 'Team_y'], axis = 1, inplace = True)
-
-
-### Rename column header ###
-combined.rename(columns = {'Team_x':'Team'}, inplace=True)
-
-combined = combined.astype({"KPrank":'int', "Champion":'int'})
+combined = pd.merge(df, df2, on= 'Year', how= 'outer', suffixes=['_l','_r'])
 
 print(combined)
-
-
 
